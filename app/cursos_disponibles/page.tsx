afd06252bbf5808c5fa1ea1f73e5adf33355d02f
Martin <telaf1997@gmail.com>
2023-08-17 14:59:56 -0300

    nuevos cambios getstatic

--- app/cursos_disponibles/page.tsx
+++ app/cursos_disponibles/page.tsx
@@ -2,19 +2,12 @@ import React from 'react'
 import { GetCourses } from '@/types/global'
 import Image from 'next/image'
 
-async function getData() {
-    const res = await fetch("https://dev-balanzuniversity-api.balpays.com/courses", { next: { revalidate: 60 } })
-    return res.json()
-}
-
-export default async function Page() {
-
-    const data: GetCourses = await getData()
+export default async function Page({ posts }) {
 
     return (
         <div className='flex-1 grid grid-cols-2 overflow-auto py-[10px] my-[10px]'>
             {
-                data.courses.map(item => {
+                posts.courses.map(item => {
                     return <div key={item.id} className='border-blue_balanz border-[1px] m-[20px] flex rounded-[30px] p-[20px]'>
                         <div className='px-[20px]'>
                             <div className='text-gray_letter_footer font-[700]'>{item.name}</div>
@@ -26,4 +19,15 @@ export default async function Page() {
             }
         </div>
     )
+}
+
+export async function getStaticProps() {
+    const res = await fetch('https://dev-balanzuniversity-api.balpays.com/courses')
+    const posts = await res.json()
+    console.log(posts)
+    return {
+        props: {
+            posts,
+        }
+    }
 }
